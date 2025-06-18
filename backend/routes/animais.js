69cc70b03584737d03b29104cf0a7c351300720c
mariajlsousa <m-sousa_19@hotmail.com>
2025-06-18 20:39:53 +0100

    Últimas alterações 4

--- backend/routes/animais.js
+++ backend/routes/animais.js
@@ -72,7 +72,7 @@ router.post('/', upload.single('foto'), async (req, res) => {
          $8,$9,$10,$11,$12,$13,
          $14,$15,$16,$17,$18,$19,$20,$21,$22,
          $23,$24,$25,$26,$27,$28,$29,$30,$31,$32,
-         $33,$34,$35,$36
+         $33,$34,$35,$36,$37
        ) RETURNING *`,
       [
         nome, especie, chip, doencas || null, entrada || null, saida || null,
